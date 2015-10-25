e1a6e8f2f9d75cfd0bb79c797d7da5c02c342d4e
Cort Pugh <pughck@rose-hulman.edu>
2015-10-25 15:59:35 -0400

    first commit

---
+++ BasicStorm/src/main/java/edu/rosehulman/pughck/ExclamationBolt.java
@@ -0,0 +1,36 @@
+package edu.rosehulman.pughck;
+
+import java.util.Map;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.IRichBolt;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+public class ExclamationBolt extends BaseRichBolt {
+
+	OutputCollector _collector;
+
+	@Override
+	public void execute(Tuple tuple) {
+
+		_collector.emit(tuple, new Values(tuple.getString(0) + "!!!"));
+		_collector.ack(tuple);
+	}
+
+	@Override
+	public void prepare(Map map, TopologyContext context, OutputCollector collector) {
+
+		_collector = collector;
+	}
+
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+
+		declarer.declare(new Fields("word"));
+	}
+}
