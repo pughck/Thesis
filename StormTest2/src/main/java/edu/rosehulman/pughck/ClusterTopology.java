61fa85f169506f87f613def5717e1f0e77264ea2
Cort Pugh <pughck@rose-hulman.edu>
2015-11-08 00:00:59 -0500

    updated - writes to hdfs in different directories by company name

--- StormTest2/src/main/java/edu/rosehulman/pughck/ClusterTopology.java
+++ StormTest2/src/main/java/edu/rosehulman/pughck/ClusterTopology.java
@@ -1,13 +1,10 @@
 package edu.rosehulman.pughck;
 
-import org.apache.storm.hdfs.bolt.HdfsBolt;
-import org.apache.storm.hdfs.bolt.rotation.NoRotationPolicy;
-import org.apache.storm.hdfs.bolt.sync.CountSyncPolicy;
-
 import backtype.storm.Config;
 import backtype.storm.StormSubmitter;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.tuple.Fields;
 
 public class ClusterTopology {
 
@@ -23,13 +20,17 @@ public class ClusterTopology {
 				"nWComrkhNlHYKVE2SjCb2D1roLzNog1NNDEh5s98c9i6KJJ6XT",
 				"4041041357-rkWibnDMhQSwJD1g5iOCsJae2J56Ni4XGbbOVe9", "XaDM2EVfh3om2uWsUD6sWVCeOERFgyDqHQAb6FfwSUUix");
 
-		HdfsBolt bolt = new HdfsBolt().withFsUrl("hdfs://hadoop-ckp-1.csse.rose-hulman.edu:8020")
-				.withRecordFormat(new ClusterRecordFormat()).withFileNameFormat(new ClusterFileNameFormat())
-				.withSyncPolicy(new CountSyncPolicy(1000)).withRotationPolicy(new NoRotationPolicy());
+		// HdfsBolt bolt = new
+		// CustomHdfsBolt().withFsUrl("hdfs://hadoop-ckp-1.csse.rose-hulman.edu:8020")
+		// .withRecordFormat(new ClusterRecordFormat()).withFileNameFormat(new
+		// ClusterFileNameFormat())
+		// .withSyncPolicy(new CountSyncPolicy(1000)).withRotationPolicy(new
+		// NoRotationPolicy());
 
 		builder.setSpout("tweetSpout", spout, 1);
-		builder.setBolt("clusterBolt", bolt, 1).globalGrouping("tweetSpout");
+		builder.setBolt("groupBolt", new GroupingBolt(), 1).shuffleGrouping("tweetSpout");
+		builder.setBolt("hdfsBolt", new MyHdfsBolt(), 1).fieldsGrouping("groupBolt", new Fields("company"));
 
-		StormSubmitter.submitTopology("cluster2", conf, builder.createTopology());
+		StormSubmitter.submitTopology("topology", conf, builder.createTopology());
 	}
 }
